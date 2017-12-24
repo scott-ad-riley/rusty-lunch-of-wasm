4ccfbf086afb5ea1bf8077d5fd136eebf0a8e2cd
Scott Riley <scott.riley111@gmail.com>
2017-12-24 13:51:04 +0000

    panics on random stuff

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
 #[macro_use]
 extern crate stdweb;
-extern crate sha1;
+extern crate rand;
 
-use sha1::Sha1;
+use rand::Rng;
 
-fn hash( string: String ) -> String {
-    let mut hasher = Sha1::new();
-    hasher.update( string.as_bytes() );
-    hasher.digest().to_string()
+static FOOD_PLACES: [&str; 16] = [
+    "Bar Burrito",
+    // "Bento Ya",
+    "Dough",
+    "Herbivore Kitchen",
+    "Hula",
+    "Let us eat",
+    // "Maki and Ramen",
+    "Mama's",
+    "Pizza Express",
+    "Pumpkin Brown",
+    "Redbox",
+    "Söderberg",
+    "Taquito",
+    "The Red Squirrel",
+    "Ting Thai Caravan",
+    "Wagamama",
+    "Wildman Wood",
+    "Zizzi's",
+];
+
+fn choose() -> String {
+    let mut rng = rand::thread_rng();
+    let choice = rng.choose(&FOOD_PLACES).unwrap();
+    String::from("blah")
 }
 
 fn main() {
     stdweb::initialize();
 
     js! {
-        Module.exports.sha1 = @{hash};
+        Module.exports.choose = @{choose};
     }
 }
